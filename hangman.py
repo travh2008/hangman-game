c7b699d92ce714ff93068b2977f4baf5ad52f825
Travis <30766297+travh2008@users.noreply.github.com>
2022-06-27 00:26:02 -0400

    Add files via upload

--- hangman.py
+++ hangman.py
@@ -1,4 +1,10 @@
 import random as r
+import hangman_words
+import hangman_art as art
+import os
+
+
+print(art.logo)
 
 #choose a word from list randomly
 word_list = ["aardvark", "baboon", "camel"]
@@ -8,70 +14,12 @@ word_choice = word_list[word_choice_num]
 # #set array for each word with underscores
 word_length = len(word_choice)
 
-print(f"The word is {word_choice} and it has {word_length} characters.")
+print(f"Psst... The word is {word_choice}")
 # #create underscore for length of word
 display=[]
 
 # # number of player lives is in variable 
 lives = 6 
-
-# #art
-stages = ['''
-  +---+
-  |   |
-  O   |
- /|\  |
- / \  |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
- /    |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|   |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
-  |   |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
-      |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-      |
-      |
-      |
-      |
-=========
-''']
 
 # #set initial display of underscores with length of word
 for i in range(0,word_length):
@@ -85,12 +33,19 @@ end_of_game = False
 
 while end_of_game != True:
   guess = input("Guess a letter: ").lower()
+  clear = os.system('cls')
+
+  #user enters letter already in game
+  if guess in display:
+    print(f"You've already guessed {guess} correctly!")
 
   #check guessed letter
   for i in range(0,word_length):
     if guess == word_choice[i]:
       display[i] = word_choice[i]
   print("Good guess!")
+  
+
     
   #if guess is not in word
   #then reduce lives by one
@@ -103,10 +58,14 @@ while end_of_game != True:
     elif lives > 0:
       print(f"Wrong guess. You have {lives} lives left.")
   print(display)
-  print(stages[lives])
+  print(art.stages[lives])
 
   #Win condition
   if display.count("_") == 0:
     end_of_game = True
     print("You Win")
-
+  
+  #End program condition
+  if guess == "1":
+    end_of_game = True
+    print("Game ended")
